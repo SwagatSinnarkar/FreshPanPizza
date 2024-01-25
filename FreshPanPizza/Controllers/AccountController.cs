6e65e0e5efa6bf0307f88b9c04f5203c78536d52
SwagatSinnarkar <swagatsinnarkar@gmail.com>
2024-01-25 13:09:47 +0530

    new changes updated

--- FreshPanPizza/Controllers/AccountController.cs
+++ FreshPanPizza/Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace FreshPanPizza.Controllers
                     }
                     else if (user.Roles.Contains("User"))
                     {
-                        return RedirectToAction("Index", "Dashboard", new { area = "User" });
+                        return RedirectToAction("Index", "Dashboard", new { area = "User" });                   
                     }
                 }
             }
--- FreshPanPizza/Controllers/HomeController.cs
+++ FreshPanPizza/Controllers/HomeController.cs
@@ -1,10 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreshPanPizza.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FreshPanPizza.Controllers
 {
     public class HomeController : Controller
     {
+        ICatalogService _catalogService;
+
+        public HomeController(ICatalogService catalogService)
+        {
+                _catalogService = catalogService;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Veg Pizza
+        public IActionResult VegPizza(int itemType)
+        {
+            var items = _catalogService.GetItems(itemType); 
+            return View(items);
+        }
+        //Non-Veg Pizza
+        public IActionResult NonVegPizza()
+        {
+            return View();
+        }
+        //Beverages
+        public IActionResult Beverages()
+        {
+            return View();
+        }
+        //Sides
+        public IActionResult Sides()
         {
             return View();
         }
