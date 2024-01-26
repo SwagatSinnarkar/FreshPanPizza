eed0695d3fd087dba1cfbfada2db64ba2f378d71
SwagatSinnarkar <swagatsinnarkar@gmail.com>
2024-01-26 11:59:26 +0530

    New Changes completed.

--- FreshPanPizza/Areas/User/Controllers/DashboardController.cs
+++ FreshPanPizza/Areas/User/Controllers/DashboardController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreshPanPizza.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FreshPanPizza.Areas.User.Controllers
 {
     public class DashboardController : BaseController
     {
+        ICatalogService _catalogService;
+
+        public DashboardController(ICatalogService catalogService)
+        {
+            _catalogService = catalogService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        //Veg Pizza
+        public IActionResult VegPizza(int itemType, int categoryType)
+        {
+            var items = _catalogService.GetItems(itemType, categoryType);
+            return View(items);
+        }
+
+        //Non-Veg Pizza
+        public IActionResult NonVegPizza(int itemType, int categoryType)
+        {
+            var items = _catalogService.GetItems(itemType, categoryType);
+            return View(items);
+        }
+
+        //Beverages
+        public IActionResult Beverages(int itemType, int categoryType)
+        {
+            var items = _catalogService.GetItems(itemType, categoryType);
+            return View(items);
+        }
+
+        //Sides
+        public IActionResult Sides(int itemType, int categoryType)
+        {
+            var items = _catalogService.GetItems(itemType, categoryType);
+            return View(items);
         }
     }
 }
---
+++ FreshPanPizza/ViewComponents/PizzaMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using FreshPanPizza.Entities;
+using FreshPanPizza.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Model.Tree;
+
+namespace FreshPanPizza.ViewComponents
+{
+    public class PizzaMenuViewComponent : ViewComponent
+    {
+        ICatalogService _catalogService;
+
+        public PizzaMenuViewComponent(ICatalogService catalogService)
+        {
+            _catalogService = catalogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Views/Shared/ViewComponentLayouts/_PizzaMenuIndex.cshtml");
+        }
+    }
+}
