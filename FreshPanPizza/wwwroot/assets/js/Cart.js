04d3348e1fcdf98dc55b19539a0bd6a49ea314ca
SwagatSinnarkar <swagatsinnarkar@gmail.com>
2024-02-01 00:33:33 +0530

    new updates

---
+++ FreshPanPizza/wwwroot/assets/js/Cart.js
@@ -0,0 +1,24 @@
+﻿function AddToCart(ItemId, Name, UnitPrice, Quantity) {
+
+    $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        url: '/Cart/AddToCart/' + ItemId + "/" + UnitPrice + "/" + Quantity,
+        success: function (d) {
+
+            var data = JSON.parse(d);
+            if (data.Items.length > 0) {
+                $('.noti_Counter').text(data.Items.length);
+                var message = '<strong>' + Name + '</strong> Added to <a>Cart</a> Successfully!';
+                $('#liveToast > .toast-body').html(message);
+                $('#liveToast').toast("show");
+                setTimeout(function () {
+                    $('#liveToast').toast("hide");
+                }, 4000);
+            }
+        },
+        error: function (result) {
+        }
+    });
+}
+
